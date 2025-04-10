42f081d686f352c9a37590b951a056393e41f2e2
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2025-04-10 09:06:29 +0200

    raft is looking good
    Signed-off-by: Gergely Brautigam <182850+Skarlso@users.noreply.github.com>

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ To run the entire demo, please execute `./run.sh`.
 
 - got to use one of the cooler generators like vault or an AWS one
     - note, binary download for vault here: https://developer.hashicorp.com/vault/install
+
+
+https://releases.hashicorp.com/vault/1.19.1/vault_1.19.1_linux_amd64.zip
+https://releases.hashicorp.com/vault/1.19.1/vault_1.19.1_darwin_amd64.zip
+
+```
+curl -L https://releases.hashicorp.com/vault/1.19.1/vault_1.19.1_darwin_amd64.zip -o vault.zip && unzip vault.zip -d bin && rm vault.zip
+```
--- run.sh
+++ run.sh
@@ -4,8 +4,11 @@ set -e
 
 BIN="bin"
 mkdir -p "${BIN}"
+kind="${BIN}/kind"
+vault="${BIN}/vault"
+cluster_name=rotate-cluster
 
-OS=$(uname)
+OS=$(uname | tr '[:upper:]' '[:lower:]')
 
 download-kind() {
     if [[ -f "${BIN}/kind" ]]; then
@@ -14,21 +17,7 @@ download-kind() {
     fi
 
     echo "fetching kind"
-    case "$OS" in
-    "Linux")
-        # Download the Linux binary (adjust URL to your needs)
-        curl -Lo "${BIN}/kind" https://kind.sigs.k8s.io/dl/v0.27.0/kind-linux-amd64
-        ;;
-    "Darwin")
-        # Download the macOS binary (adjust URL to your needs)
-        curl -Lo "${BIN}/kind" https://kind.sigs.k8s.io/dl/v0.27.0/kind-darwin-amd64
-        ;;
-    *)
-        echo "Unsupported operating system: $OS"
-        exit 1
-        ;;
-    esac
-
+    curl -Lo "${BIN}/kind" https://kind.sigs.k8s.io/dl/v0.27.0/kind-"${OS}"-amd64
     chmod +x "${BIN}/kind"
 }
 
@@ -39,47 +28,67 @@ download-vault() {
     fi
 
     echo "fetching vault"
-    case "$OS" in
-    "Linux")
-        # Download the Linux binary (adjust URL to your needs)
-        curl -Lo "${BIN}/kind" https://kind.sigs.k8s.io/dl/v0.27.0/kind-linux-amd64
-        ;;
-    "Darwin")
-        # Download the macOS binary (adjust URL to your needs)
-        curl -Lo "${BIN}/kind" https://kind.sigs.k8s.io/dl/v0.27.0/kind-darwin-amd64
-        ;;
-    *)
-        echo "Unsupported operating system: $OS"
-        exit 1
-        ;;
-    esac
-
+    curl -L https://releases.hashicorp.com/vault/1.19.1/vault_1.19.1_"${OS}"_amd64.zip -o vault.zip && unzip vault.zip -d "${BIN}" && rm vault.zip
     chmod +x "${BIN}/vault"
 }
 
+setup-vault() {
+    kubectl apply -f vault-role.yaml
+    kubectl apply -f vault-role-binding.yaml
+    helm repo add hashicorp https://helm.releases.hashicorp.com
+    helm repo update
+    helm install vault hashicorp/vault --values helm-vault-raft-values.yaml
+
+    echo "Waiting for pods with label 'app.kubernetes.io/name: vault' to exist..."
+    until kubectl get pods -l "app.kubernetes.io/name=vault" --no-headers | grep -q .; do
+        sleep 2
+    done
+
+    kubectl wait --for=jsonpath='{.status.phase}'=Running pod/vault-0
+
+    kubectl exec vault-0 -- vault operator init \
+        -key-shares=1 \
+        -key-threshold=1 \
+        -format=json >cluster-keys.json
+
+    vault_token=$(jq -r ".unseal_keys_b64[]" cluster-keys.json)
+    kubectl exec vault-0 -- vault operator unseal "${vault_token}"
+
+    echo "waiting for vault to become ready after unsealing"
+    kubectl wait --for=condition=Ready=true Pod/vault-0 --timeout=300s
+}
+
 kind-create-test-cluster() {
-    kind create cluster --name rotate-cluster
+    existing_clusters=$(kind get clusters)
+
+    # Check if the cluster exists
+    if echo "$existing_clusters" | grep -q "$cluster_name"; then
+        echo "Cluster '$cluster_name' exists."
+    else
+        ${kind} create cluster --name "${cluster_name}" || true
+    fi
 }
 
 deploy-external-secrets-operator() {
     helm repo add external-secrets https://charts.external-secrets.io
-    helm install external-secrets external-secrets/external-secrets -n external-secrets --create-namespace
+    helm repo update
+    helm install external-secrets external-secrets/external-secrets -n external-secrets --create-namespace || true
+}
+
+spinner() {
+    local pid=$1
+    local delay=0.2
+    local spinstr='|/-\\'
+    while kill -0 "$pid" 2>/dev/null; do
+        local temp=${spinstr#?}
+        printf " [%c]  \r" "$spinstr"
+        spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+    done
+    echo "    "
 }
 
 check-external-secrets-operator-deployment() {
-    spinner() {
-        local pid=$1
-        local delay=0.2
-        local spinstr='|/-\\'
-        while kill -0 "$pid" 2>/dev/null; do
-            local temp=${spinstr#?}
-            printf " [%c]  \r" "$spinstr"
-            spinstr=$temp${spinstr%"$temp"}
-            sleep $delay
-        done
-        echo "    "
-    }
-
     kubectl wait --for=condition=Available=true Deployment/external-secrets -n external-secrets --timeout=300s &
     spinner $!
     kubectl wait --for=condition=Available=true Deployment/external-secrets-cert-controller -n external-secrets --timeout=300s &
@@ -110,4 +119,6 @@ check-external-secrets-operator-deployment
 
 echo "Operator deployed."
 
-echo "Applying secrets and other manifests."
+echo "Setting up vault."
+
+setup-vault
